a8a579b09651d4c90d9bd38ebebe2c5865c2df0e
Bryce Coleman <coleman.bryce@gmail.com>
2018-02-06 12:58:43 -0500

    Setting up MongoDB client

--- js/search.js
+++ js/search.js
@@ -12,41 +12,50 @@ function search() {
 
 function createObj(id) {
   templateArr = [
-    ["For teachers, IXL Analytics offers real-time...", "analytics"],
-    ["You will find these skills in your analytics...", "skills"],
-    ["This subject will now appear in your Analytics...", "analytics"],
-    ["Please provide me with the skill you were...", "skills"],
-    ["To sign in to your account, please...", "signingIn"],
-    ["Click on the Analytics tab at the top...", "analytics"],
-    ["The SmartScore will adjust...", "smartScore"],
-    ["To sign in to your account...","signingIn"],
-    ["This subject will now appear in your Analytics...", "analytics"],
-    ["The SmartScore will adjust...", "smartScore"],
-    ["You will find these skills in your analytics...", "skills"],
-    ["Click on the Analytics tab at the top...", "analytics"],
-    ["To access the Trouble spots report, please...", "analytics"],
-    ["To sign in to your account, please...", "signingIn"],
-    ["Please provide me with the skill you were...", "skills"],
-    ["Please go to ixl.com to sign in...","signingIn"]
+    ["For teachers, IXL Analytics offers real-time...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ", "analytics"],
+    ["You will find these skills in your analytics...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ", "skills"],
+    ["To view this Analytics report, select the subject from...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ", "analytics"],
+    ["Please provide me with the skill you were...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ", "skills"],
+    ["To sign in to your account, please...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ", "signingIn"],
+    ["Click on the Analytics tab at the top...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ", "analytics"],
+    ["The SmartScore will adjust...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ", "smartScore"],
+    ["To sign in to your account...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ","signingIn"],
+    ["This subject will appear in your Analytics...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ", "analytics"],
+    ["The SmartScore will adjust...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ", "smartScore"],
+    ["You will find these skills in your analytics...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ", "skills"],
+    ["Click on the Analytics tab at the top...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ", "analytics"],
+    ["To access the this report, please...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ", "analytics"],
+    ["To sign in to your account, please...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ", "signingIn"],
+    ["Please provide me with the skill you were...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ", "skills"],
+    ["Please go to ixl.com to sign in...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu nisl, mollis eu efficitur ac, gravida vel lectus. Praesent feugiat aliquet velit, ","signingIn"]
   ]
   var li = document.createElement("LI");
-  var a = document.createElement("p");
+  var i = document.createElement("i");
+  var p = document.createElement("p");
   var h3 = document.createElement("h3");
   var h5 = document.createElement("h5");
   var br = document.createElement("br");
-  var nodeTitle = document.createTextNode("Template " + id + "." + Math.floor(Math.random() * 20));
+  var tags = document.createElement("p");
+  var nodeTitle = document.createTextNode("Template " + id + "." + Math.floor(Math.random() * 20) + '  ');
   var nodeLastUpdatedDate = document.createTextNode('Last updated Jan 31, 2018');
   var nodeTemplate = document.createTextNode(templateArr[id - 1][0]);
+  var nodeTags = document.createTextNode('Tags {password, reset, analytics, SmartScore} ');
   li.id = elementId;
-  a.classList.add(templateArr[id -1][1]);
+  p.classList.add(templateArr[id -1][1]);
   h3.classList.add(templateArr[id -1][1]);
-  h3.appendChild(nodeTitle)
-  h5.appendChild(nodeLastUpdatedDate)
+  i.classList.add("glyphicon");
+  i.classList.add("glyphicon-copy");
+  tags.classList.add('tags');
+  h3.appendChild(nodeTitle);
+  h5.appendChild(nodeLastUpdatedDate);
+  tags.appendChild(nodeTags);
   li.appendChild(h3);
   li.appendChild(h5);
-  li.appendChild(a);
-  a.appendChild(br)
-  a.appendChild(nodeTemplate)
+  li.appendChild(p);
+  p.appendChild(br);
+  p.appendChild(nodeTemplate);
+  li.appendChild(tags);
+  h3.appendChild(i);
   return li;
 }
 
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
-})
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
@@ -11,3 +8,23 @@ app.get('/', (req, res) => {
 
 app.use(express.static(__dirname + '/css'));
 app.use(express.static(__dirname + '/js'));
+
+var db;
+
+const MongoClient = require('mongodb').MongoClient
+MongoClient.connect('mongodb://helios-user:helios-user@ds225608.mlab.com:25608/helios-drafts', (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('helios-drafts')
+  app.listen(3000, function() {
+    console.log('listening on 3000')
+  })
+})
+
+app.post('/drafts', (req, res) => {
+  db.collection('drafts').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
