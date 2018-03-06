13a029b4f935747e973f77b2ec2eff2e331768f7
Bryce Coleman <coleman.bryce@gmail.com>
2018-03-06 12:48:15 -0500

    Add 'users' database

--- server.js
+++ server.js
@@ -2,61 +2,65 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
- app.use(bodyParser.json());
- app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/js'));
+app.use(express.static(__dirname + '/public/'));
 
-// Sets up Mongo Client
-var db
-const MongoClient = require('mongodb').MongoClient
-MongoClient.connect('mongodb://helios-user:helios-user@ds125628.mlab.com:25628/templates', (err, client) => {
+// Connects to 'users' and 'templates' databases
+const templatesClient = require('mongodb').MongoClient
+const usersClient = require('mongodb').MongoClient
+
+var templatesDb;
+var usersDb;
+
+templatesClient.connect('mongodb://templates-admin:templates-admin@ds125628.mlab.com:25628/templates', (err, client) => {
   if (err) return console.log(err)
-  db = client.db('templates')
+  templatesDb = client.db('templates')
   app.listen(3000, function() {
-    console.log('listening on 3000')
+    console.log('templates connected on 3001')
   })
 });
 
-
-
-app.delete('/quotes', (req, res) => {
-  console.log(req.body.name)
-  db.collection('templates').findOneAndDelete({name: req.body.name},
-  (err, result) => {
-    if (err) return res.send(500, err)
-    res.send({message: 'A darth vadar quote got deleted'})
+usersClient.connect('mongodb://users-admin:users-admin@ds257858.mlab.com:57858/users', (err, client) => {
+  if (err) return console.log(err)
+  users = client.db('templates')
+  app.listen(3001, function() {
+    console.log('users connected on 3000')
   })
 });
-
-app.set('view engine', 'ejs')
-
-app.use(express.static(__dirname + '/css'))
-app.use(express.static(__dirname + '/js'))
-app.use(express.static(__dirname + '/public/'))
-
-app.delete('/quotes', (req, res) => {
-  db.collection('templates').findOneAndDelete({name: '2'},
-  (err, result) => {
-    if (err) return res.send(500, err)
-    res.send({message: 'A darth vadar quote got deleted'})
-  })
-});
-
-
-
 
 // Posts form data to Mongo Client
 app.post('/add-template', (req, res) => {
-  db.collection('templates').save(req.body, (err, result) => {
+  templatesDb.collection('templates').save(req.body, (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
     res.redirect('/')
   })
 });
 
-app.put('/update', (req, res) => {
+app.delete('/quotes', (req, res) => {
+  console.log(req.body.name)
+  templatesDb.collection('templates').findOneAndDelete({
+      name: req.body.name
+    },
+    (err, result) => {
+      if (err) return res.send(500, err)
+      res.send({
+        message: 'A darth vadar quote got deleted'
+      })
+    })
+});
+
+
+/*app.put('/update', (req, res) => {
   console.log(req.body.findName);
   console.log(req.body.newName);
-  db.collection('templates')
+  templatesDb.collection('templates')
     .findOneAndUpdate({
       name: req.body.findName
     }, {
@@ -68,9 +72,10 @@ app.put('/update', (req, res) => {
       res.send(result)
     })
 });
+//still working on */
 
 app.get('/', (req, res) => {
-  db.collection('templates').find().toArray((err, result) => {
+  templatesDb.collection('templates').find().toArray((err, result) => {
     if (err) return console.log(err)
     // renders index.ejs
     res.render('index.ejs', {
