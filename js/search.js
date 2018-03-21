7af427f96239651bc6d74f852222a8f22a8fcca7
Bryce Coleman <coleman.bryce@gmail.com>
2018-03-21 10:14:35 -0400

    updated search feature so search functions when user deletes word

--- js/search.js
+++ js/search.js
@@ -1,32 +1,51 @@
 function search() {
-  pass = true;
-  elementId = "element"
-}
-
-function filter() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("search-bar");
-  filter = input.value.toUpperCase().split(' ');
+  filterArr = input.value.toUpperCase().split(' ').clean('');
   ul = document.getElementById("data-table");
   li = ul.getElementsByTagName("li");
+  // TODO: Remove Category and Type names from search
+  // Loop through each list element
   for (k = 0; k < li.length; k++) {
-    content = li[k].textContent;
-    console.log(content);
-    for (var m = 0; m < filter.length; m++) {
-      if (content.toUpperCase().indexOf(filter[m]) > -1) {
-        li[k].style.display = "";
+    var listElement = li[k];
+    content = listElement.textContent; // convert element to text
+    // if search bar is blank, display all
+    if (filterArr.length == 0) {
+      listElement.style.display = '';
+
+    } else if (listElement.style.display == 'none') {
+      if (test(filterArr, listElement)) {
+        listElement.style.display = '';
+      }
+
+    } else {
+      if (test(filterArr, listElement)) {
+        listElement.style.display = '';
       } else {
-        li[k].style.display = "none";
+        listElement.style.display = 'none';
       }
     }
-    //for(var i = 0; i < elements.length; i++) {
-    //     var current = elements[i];
-    //     if(current.children.length === 0 && current.textContent.replace(/ |\n/g,'') !== '') {
-    // Check the element has no children && that it is not empty
-    //         array.push(current.textContent);
-    //      }
-    //  }
   }
 }
 
-filter();
+function test(filterArr, listElement) {
+  for (var m = 0; m < filterArr.length; m++) {
+    // If content of element doesn't match
+    if (content.toUpperCase().indexOf(filterArr[m]) == -1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+Array.prototype.clean = function(value) {
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] == value) {
+      this.splice(i, 1);
+      i--;
+    }
+  }
+  return this;
+};
+
+search();
