5399d72f50d1f3fdfb3389473c56a837a43635b1
Bryce Coleman <coleman.bryce@gmail.com>
2018-03-21 10:12:31 -0400

    Added update feature, fixed remove/add feature

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const templatesClient = require('mongodb').MongoClient;
+const itemsClient = require('mongodb').MongoClient;
 const usersClient = require('mongodb').MongoClient;
 
 app.set('view engine', 'ejs');
@@ -14,51 +14,72 @@ app.use(express.static(__dirname + '/css'));
 app.use(express.static(__dirname + '/js'));
 app.use(express.static(__dirname + '/public/'));
 
-// Connects to 'users' and 'templates' databases
-var templatesDb;
+// Connects to 'users' and 'items' databases
+var itemsDb;
 var usersDb;
 
-templatesClient.connect('mongodb://templates-admin:templates-admin@ds125628.mlab.com:25628/templates', (err, client) => {
+itemsClient.connect('mongodb://templates-admin:templates-admin@ds125628.mlab.com:25628/templates', (err, client) => {
   if (err) return console.log(err)
-  templatesDb = client.db('templates')
+  itemsDb = client.db('templates')
   app.listen(3000, function() {
-    console.log('templatesDb connected on 3000')
+    console.log('itemsDb connected on 3000')
   })
 });
 
 usersClient.connect('mongodb://users-admin:users-admin@ds257858.mlab.com:57858/users', (err, client) => {
   if (err) return console.log(err)
-  users = client.db('templates')
-  app.listen(3001
-    , function() {
+  users = client.db('users')
+  app.listen(3001, function() {
     console.log('usersDb connected on 3001')
   })
 });
 
 // Posts form data to Mongo Client
-app.post('/add-template', (req, res) => {
-  templatesDb.collection('templates').save(req.body, (err, result) => {
+app.post('/add', (req, res) => {
+  itemsDb.collection('templates').save(req.body, (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
-    res.redirect('/templates')
+    res.redirect('/items')
   })
 });
 
-app.delete('/quotes', (req, res) => {
-  console.log(req.body.name)
-  templatesDb.collection('templates').findOneAndDelete({
-      name: req.body.name
+app.delete('/remove', (req, res) => {
+  itemsDb.collection('templates').findOneAndDelete({
+      id: req.body.id
     },
     (err, result) => {
       if (err) return res.send(500, err)
       res.send({
-        message: 'A darth vadar quote got deleted'
+        message: 'Removed'
       })
     })
 });
 
-app.get('/templates', (req, res) => {
-  templatesDb.collection('templates').find().toArray((err, result) => {
+app.put('/update', (req, res) => {
+  console.log('ID: ' + req.body.id);
+  console.log('Name: ' + req.body.name);
+  itemsDb.collection('templates')
+    .findOneAndUpdate({
+      id: req.body.id
+    }, {
+      $set: {
+        name: req.body.name,
+        body: req.body.body,
+        tags: req.body.tags
+      }
+    }, {
+      sort: {
+        _id: -1
+      },
+      upsert: true
+    }, (err, result) => {
+      if (err) return res.send(err)
+      res.send(result)
+    })
+});
+
+app.get('/items', (req, res) => {
+  itemsDb.collection('templates').find().toArray((err, result) => {
     if (err) return console.log(err)
     // renders index.ejs
     res.render('index.ejs', {
@@ -68,7 +89,7 @@ app.get('/templates', (req, res) => {
 });
 
 app.get('/users', (req, res) => {
-  templatesDb.collection('users').find().toArray((err, result) => {
+  itemsDb.collection('users').find().toArray((err, result) => {
     if (err) return console.log(err)
     // renders users.ejs
     res.render('users.ejs', {
@@ -76,20 +97,3 @@ app.get('/users', (req, res) => {
     })
   })
 });
-
-/*app.put('/update', (req, res) => {
-  console.log(req.body.findName);
-  console.log(req.body.newName);
-  templatesDb.collection('templates')
-    .findOneAndUpdate({
-      name: req.body.findName
-    }, {
-      $set: {
-        name: req.body.newName,
-      }
-    }, (err, result) => {
-      if (err) return res.send(err)
-      res.send(result)
-    })
-});
-//still working on */
