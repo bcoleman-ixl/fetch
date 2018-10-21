9a9fe42a3d8f3353a0477fbd16443ed8d15a4096
Bryce Coleman <coleman.bryce@gmail.com>
2018-10-20 22:12:56 -0400

    increased search speed by searching when user finishes typing.

--- js/search.js
+++ js/search.js
@@ -1,63 +1,68 @@
 var searchLogArr = new Array();
+let scruffyImage = document.getElementById('scruffy');
+let scruffyMessage = document.getElementById('scruffyMessage');
+let ul = document.getElementById('data-table');
+let li = ul.getElementsByClassName('template');
 
 function search() {
   let input = document.getElementById('search-bar');
-  let filterArr = input.value.toUpperCase().split(' ').clean('');
-  let ul = document.getElementById('data-table');
-  let li = ul.getElementsByClassName('template');
-  if (filterArr == '') {
-    console.log(filterArr);
-    for (var i = 0; i < li.length; i++) {
-      let item = li[i];
-      item.style.display = '';
-    }
-  }
-  for (k = 0; k < li.length; k++) {
-    // Loop through each list item
-    let item = li[k];
-    // convert item to text and replace date with blank space
-    content = item.textContent.replace(/[A-Z]*[a-z][a-z]\s\d*,\s\d{4}/, '');
+  // If the user has entered one character, do not search.
+  if (input.value.length != 0) {
+    let filterArr = input.value.toUpperCase().split(' ').clean('');
+    if (filterArr == '') {
+      for (var i = 0; i < 25; i++) {
+        let item = li[i];
+        item.style.display = '';
+      }
+      document.getElementById('templateCount').innerHTML = li.length;
+    } else {
+      for (k = 0; k < li.length; k++) {
 
-    // If search bar is blank, display this element
-    if (filterArr.length == 0) {
-      item.style.display = '';
-      // If item is hidden, test to see if its content matches filter
-    } else if (item.style.display == 'none') {
-      if (test(filterArr, item)) {
+        // Loop through each list item
+        let item = li[k];
+
+        // convert item to text and replace date with blank space
+        content = item.textContent.replace(/[A-Z]*[a-z][a-z]\s\d*,\s\d{4}/, '');
+
         // If item content matches filter, display item
-        item.style.display = '';
+        if (item.style.display == 'none' && test(filterArr, content)) {
+          item.style.display = '';
+
+          // If item is not equal to 'none' and content matches filter, show item
+        } else if (test(filterArr, content)) {
+          item.style.display = '';
+
+          // If item content doesn't match filter, hide item
+        } else {
+          item.style.display = 'none';
+        }
       }
-      // If item is displayed to user, check to see if its content matches filter
-    } else {
-      if (test(filterArr, item)) {
-        // If item content matches filter, show item
-        item.style.display = '';
-      } else {
-        // If item content doesn't match filter, hide item
-        item.style.display = 'none';
+      /**
+       * [display description]
+       scruffyImage.style.display = 'none';
+       scruffyMessage.style.display = 'none';
+       scruffyImage.style.display = 'inline';
+       scruffyMessage.style.display = 'inline';
+       * @type {String}
+       */
+      document.getElementById('templateCount').innerHTML = $('#data-table').children(':visible').length - 1;
+      if (($('#data-table').children(':visible').length - 1) == 0) {
+        let searchContents = document.getElementById('search-bar').value;
       }
     }
-  }
-  document.getElementById('templateCount').innerHTML = $('#data-table').children(':visible').length;
-
-  if ($('#data-table').children(':visible').length == 0) {
-    let searchContents = document.getElementById('search-bar').value;
-
-    if ($.inArray(searchContents, searchLogArr) < 0) {
-      searchLogArr.push(searchContents);
-      console.log(searchLogArr);
-      $('#test').show();
-    } else {
-
+  } else {
+    reset();
+    let filterArr = input.value.toUpperCase().split(' ').clean('');
+    if (filterArr == '') {
+      for (var i = 0; i < 24; i++) {
+        let item = li[i];
+        item.style.display = '';
+      }
     }
-  } else if ($('#data-table').children(':visible').length != 0 && searchLogArr.length > 0) {
-    updateLogs(searchLogArr[searchLogArr.length - 1]);
-    console.log('sending: ' + searchLogArr[searchLogArr.length - 1]);
-    searchLogArr = [];
   }
 }
 
-function test(filterArr, item) {
+function test(filterArr, content) {
   for (let m = 0; m < filterArr.length; m++) {
     // If at any point item content doesn't match the filter, return false
     if (content.toUpperCase().indexOf(filterArr[m]) == -1) {
@@ -68,16 +73,15 @@ function test(filterArr, item) {
   return true;
 }
 
-function showAll(){
+function reset() {
   let ul = document.getElementById('data-table');
   let li = ul.getElementsByClassName('template');
-  for (k = 0; k < li.length; k++) {
+  for (k = 0; k < 25; k++) {
     let item = li[k];
-      item.style.display = '';
-      console.log('showAll');
-      }
-        document.getElementById('templateCount').innerHTML = li.length;
-    }
+    item.style.display = '';
+  }
+  document.getElementById('templateCount').innerHTML = li.length;
+}
 
 Array.prototype.clean = function(value) {
   // Remove trailing and ending white space
@@ -89,3 +93,27 @@ Array.prototype.clean = function(value) {
   }
   return this;
 };
+
+/**
+ * Searches only when user has finished typing (when 200ms has passed without key activity)
+ * @type {Number}
+ */
+var typingTimer; //timer identifier
+var doneTypingInterval = 200; //time in ms
+var $input = $('#search-bar');
+
+//on keyup, start the countdown
+$input.on('keyup', function() {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(doneTyping, doneTypingInterval);
+});
+
+//on keydown, clear the countdown
+$input.on('keydown', function() {
+  clearTimeout(typingTimer);
+});
+
+//user is finished typing, search
+function doneTyping() {
+  search();
+}
